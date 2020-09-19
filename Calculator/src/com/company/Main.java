2aa1cba56342e453c80a17fba63f27bc04202f38
RaynurKines <kinespaev.raynur@gmail.com>
2020-09-19 09:29:54 +0600

    lab03 has been started

---
+++ Calculator/src/com/company/Main.java
@@ -0,0 +1,33 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] argsOriginal) {
+        try {
+            String consoleLine = "-1.5 + 1e-0.2";
+            String[] args = consoleLine.split(" ");
+            double x = Double.parseDouble(args[0]);
+            char operation = args[1].charAt(0);
+            double y = Double.parseDouble(args[2]);
+            System.out.println(calc(x, y, operation));
+        }catch(NumberFormatException ex){
+            System.out.println("Invalid number format");
+        }
+    }
+
+    static double calc(double x, double y, char operation) {
+        switch (operation) {
+            case '+':
+                return x + y;
+            case '-':
+                return x - y;
+            case '*':
+                return x * y;
+            case '/':
+                return x / y;
+            default:
+                return Double.NaN;
+        }
+
+    }
+}
