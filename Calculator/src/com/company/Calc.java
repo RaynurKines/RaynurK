56b51b63c8ffbae8e8f1f9d2f043444b8ccceb60
RaynurKines <kinespaev.raynur@gmail.com>
2020-09-19 11:18:01 +0600

    Division by zero

--- Calculator/src/com/company/Calc.java
+++ Calculator/src/com/company/Calc.java
@@ -1,22 +1,15 @@
 package com.company;
 
-public class Main {
+public class Calc {
 
-    public static void main(String[] argsOriginal) {
+    public static void main(String[] args) {
         try {
-            String consoleLine = "-1.5 - 1e-2";
-            //String consoleLine = "-1.5 + 1e-2";
-            //String consoleLine = "-1.5 * 1e-2";
-            //String consoleLine = "-1.5 / 1e-2";
-
-            //You stupid. It's fail
-            //String consoleLine = "-1.5 / 1e-13";
-            String[] args = consoleLine.split(" ");
             double x = Double.parseDouble(args[0]);
             char operation = args[1].charAt(0);
             double y = Double.parseDouble(args[2]);
-            if (operation == '/' && y > -1e-12 || operation == '/' && y < 1e-12) {
-                throw new IllegalArgumentException("You stupid. It's fail");
+            if (operation == '/' && y > -1e-12 && y < 1e-12) {
+                System.out.println("Division by zero");
+                return;
             }
             System.out.println(calc(x, y, operation));
         } catch (NumberFormatException ex) {
