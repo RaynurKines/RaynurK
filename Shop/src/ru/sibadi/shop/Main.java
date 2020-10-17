c0d1bc71d455982a85cc9b9891d68773ec69825a
RaynurKines <kinespaev.raynur@gmail.com>
2020-10-17 10:27:17 +0600

    product added

--- Shop/src/ru/sibadi/shop/Main.java
+++ Shop/src/ru/sibadi/shop/Main.java
@@ -1,7 +1,16 @@
 package ru.sibadi.shop;
+import ru.sibadi.shop.model.Product;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[]args){
         System.out.println("Hello World");
+        List<Product> products = new ArrayList<>();
+        products.add(new Product("Bread", 25));
+        products.add(new Product("Butter", 40));
+        System.out.println(products.get(0).getName() + " : " + products.get(0).getPrice());
+        System.out.println(products.get(1).getName() + " : " + products.get(1).getPrice());
     }
 }
