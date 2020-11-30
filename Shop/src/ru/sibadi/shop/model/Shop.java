2a787f9e8d9bbf4bae9206dfbbf55672a114d3f4
RaynurKines <kinespaev.raynur@gmail.com>
2020-11-30 14:29:35 +0600

    Version with files

--- Shop/src/ru/sibadi/shop/model/Shop.java
+++ Shop/src/ru/sibadi/shop/model/Shop.java
@@ -1,18 +1,30 @@
 package ru.sibadi.shop.model;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Shop {
-    private final List<Product> products = new ArrayList<>();
+public class Shop{
+    private final List<Product> products;
+    private final List<Customer> customers;
     private final List<Buy> purchases = new ArrayList<>();
     private Sale sale = Sale.WITHOUT;
 
     public Shop() {
+        this.products = new ArrayList<>();
         products.add(new Product("Bread", 25));
         products.add(new Product("Butter", 40));
         products.add(new Product("Milk", 70));
+        this.customers = new ArrayList<>();
+        customers.add(new Customer("Danil", 150));
+        customers.add(new Customer("Tanya", 300));
     }
+    public Shop(List<Product> products, List<Customer> customers){
+        this.products=products;
+        this.customers=customers;
+    }
+
+    public List<Customer> getCustomers() { return customers; }
 
     public List<Product> getProducts() {
         return products;
@@ -39,10 +51,50 @@ public class Shop {
         return buy;
     }
 
-    public void watchPurchases() {
-        System.out.println("\nPurchases:");
-        for (Buy buy : getPurchases()) {
-            System.out.println(buy);
+
+
+    public void writeInTxtProducts(){
+
+        try(FileWriter writer = new FileWriter("Products.txt")) {
+            for (Product product : products) {
+                String name = product.getName();
+                String price = String.valueOf(product.getPrice());
+                writer.write(name + " " + price + System.getProperty("line.separator"));
+
+            }
+        }
+        catch(IOException ex){
+
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    public void writeInTxtPurchases() {
+
+        try (FileWriter writer2 = new FileWriter("Purchases.txt")) {
+            for (Buy buy : purchases) {
+                String customer = buy.getCustomer().getName();
+                String product = buy.getProduct().getName();
+                String price = String.valueOf(buy.getProduct().getPrice());
+                writer2.write(customer + " " + product + " " + price + System.getProperty("line.separator"));
+            }
+        } catch (IOException ex) {
+
+            System.out.println(ex.getMessage());
+        }
+    }
+    public void writeInTxtCustomers() {
+
+        try (FileWriter writer3 = new FileWriter("Customers.txt")) {
+            for (Customer customer : customers) {
+                String name = customer.getName();
+                String money = String.valueOf(customer.getMoney());
+                writer3.write(name + " " + money + System.getProperty("line.separator"));
+
+            }
+        } catch (IOException ex) {
+
+            System.out.println(ex.getMessage());
         }
     }
 
